eb3e6878c2e11c3117437298023004ef7101c80b
Max Oliveira <max-oliveira@outlook.com.br>
2023-01-13 21:46:21 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require('electron');
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 600,
+        height: 600,
+        backgroundColor: "#123"
+    })
+}
+
+app.whenReady().then(() => {
+    createWindow();
+})
