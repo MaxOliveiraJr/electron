8f7add93dd745e55a6be1bc4fdc25d5f30a83d3a
Max Oliveira <max-oliveira@outlook.com.br>
2023-01-13 22:05:42 -0300

    feat: create window

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron'); 
 function createWindow() {
     const win = new BrowserWindow({
         width: 600,
         height: 600,
         backgroundColor: "#123"
-    })
+    });
+    win.loadFile("./src/index.html");
+    win.webContents.openDevTools(); 
 }
 
 app.whenReady().then(() => {
