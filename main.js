1908680ee09ee801eabecbb8e987e8b545330a86
Max Oliveira <max-oliveira@outlook.com.br>
2023-01-16 20:42:18 -0300

    feat: para mac

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
-const { app, BrowserWindow } = require('electron'); 
+const { app, BrowserWindow } = require('electron');
+
+const isDev = process.env.NODE_DEV !== undefined && process.env.NODE_DEV === "development" ? true : false;
+
+const isMac = process.platform === 'darwin' ? true : false;
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 600,
         height: 600,
-        backgroundColor: "#123"
+        backgroundColor: "#123",
+        show: false
     });
     win.loadFile("./src/index.html");
-    win.webContents.openDevTools(); 
+    if (isDev) {
+        win.webContents.openDevTools();
+    }
+    win.once('ready-to-show', () => {
+        win.show();
+    })
 }
 
 app.whenReady().then(() => {
     createWindow();
+})
+
+app.on('window-all-closed', () => {
+    console.log('Todas as Janelas fechadas');
+    if (!isMac) {
+        app.quit();
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 })
